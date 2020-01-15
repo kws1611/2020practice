da3f8c922af8af0ba317090bb708ebf81c4d92ef
eeverblue <chicken_00@nate.com>
2020-01-15 10:52:09 +0900

    comp

--- complementary.py
+++ complementary.py
@@ -4,11 +4,12 @@ import rospy
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import MagneticField
+from numpy import sqrt, pi
 import tf
+import time
 
 import smbus
 import numpy as np
-from numpy import sqrt, pi
 import os, sys
 
 def quaternionMultiplication(p0, p1, p2, p3, q0, q1, q2, q3):
@@ -48,12 +49,18 @@ class complement_Filter:
 		self.q1 = 0
 		self.q2 = 0
 		self.q3 = 0
-		self.Alpha=0.5
-		self.Beta=0.5
+		self.Alpha = 0.5
+		self.Beta = 0.5
+		self.t_prev = time.time()
 		while self.IsBias == False:
 			rospy.Subscriber("/imu_raw",Imu,self.imu_raw_data)
 			self.getBias()
-			
+
+	def calcDT(self):
+		t_now = time.time()
+		self.dt = t_now - self.t_prev
+		self.t_prev = t_now
+
 	def imu_raw_data(self,msg):
 		self.a_x=msg.linear_acceleration.x
 		self.a_y=msg.linear_acceleration.y
@@ -71,7 +78,7 @@ class complement_Filter:
 		self.g_xSum = self.g_xSum + self.g_x
 		self.g_ySum = self.g_ySum + self.g_y
 		self.g_zSum = self.g_zSum + self.g_z
-		if self.numBias == self.goalBias
+		if self.numBias == self.goalBias :
 			self.IsBias = True
 			self.g_xBias = self.g_xSum/self.goalBias
 			self.g_yBias = self.g_ySum/self.goalBias
@@ -85,9 +92,10 @@ class complement_Filter:
 	
 	def getMagnetic(self):
 		rospy.Subscriber("/mag_raw", MagneticField, self.mag_raw_data)
-		#need to do magneto correction
+		#need to do magneto calibration
 
 	def getPrediction(self):
+		self.calcDT()
 		self.getAccelGyro()
 		q0_gyro,q1_gyro,q2_gyro,q3_gyro = quaternionMultiplication(0, self.g_x, self.g_y, self.g_z, self.q0, self.q1, self.q2, self.q3)
 		q0_gyro = self.q0 - 0.5 * q0_gyro * self.dt
@@ -119,8 +127,8 @@ class complement_Filter:
 	def Mag_Correction(self):
 		self.getMagnetic()
 		beta = self.gainMag(self.m_x, self.m_y, self.m_z) * self.Beta
-		lx, ly, lz = rotate(self.m_x, self.m_y, self.m_z, self.q0, self.q1, self.q2, self.q3)
-		gamma = lx ** 2 + ly **2
+		lx, ly, lz = rotate(self.m_x, self.m_y, self.m_z, self.q0, - self.q1, - self.q2, - self.q3)
+		gamma = lx ** 2 + ly ** 2
 		q0_mag = beta * sqrt(gamma + lx * sqrt(gamma))/ sqrt(2 * gamma) + (1 - beta)
 		q1_mag = 0
 		q2_mag = 0
@@ -134,6 +142,7 @@ class complement_Filter:
 		self.q0, self.q1, self.q2, self.q3 = quaternionMultiplication(q0_gyro, q1_gyro, q2_gyro, q3_gyro, q0_acc, q1_acc, q2_acc, q3_acc)
 		q0_mag, q1_mag, q2_mag, q3_mag = self.Mag_Correction()
 		self.q0, self.q1, self.q2, self.q3 = quaternionMultiplication(self.q0, self.q1, self.q2, self.q3, q0_mag, q1_mag, q2_mag, q3_mag)
+		print(str(self.q0) + str(self.q1) + str(self.q2) + str(self.q3))
 
 if __name__=="__main__":
 	rospy.init_node("Complementary", anonymous = True)
