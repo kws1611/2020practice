a727526eab86ac24a1fb880f1d2ed9eb439fc622
eeverblue <chicken_00@nate.com>
2020-01-15 21:18:55 +0900

    comp

--- complementary.py
+++ complementary.py
@@ -13,10 +13,10 @@ import numpy as np
 import os, sys
 
 def quaternionMultiplication(p0, p1, p2, p3, q0, q1, q2, q3):
-    r0 = p0*q0 - p1*q1 - p2*q2 - p3*q3
-    r1 = p0*q1 + p1*q0 + p2*q3 - p3*q2
-    r2 = p0*q2 - p1*q3 + p2*q0 + p3*q1
-    r3 = p0*q3 + p1*q2 - p2*q1 + p3*q0
+    r0 = p0 * q0 - p1 * q1 - p2 * q2 - p3 * q3
+    r1 = p0 * q1 + p1 * q0 + p2 * q3 - p3 * q2
+    r2 = p0 * q2 - p1 * q3 + p2 * q0 + p3 * q1
+    r3 = p0 * q3 + p1 * q2 - p2 * q1 + p3 * q0
     return r0, r1, r2, r3
 
 def normalizeQuaternion(q0, q1, q2, q3):
@@ -28,9 +28,9 @@ def normalizeQuaternion(q0, q1, q2, q3):
     return q0, q1, q2, q3
 
 def rotateVectorQuaternion(x, y, z, q0, q1, q2, q3):
-    vx = (q0*q0 + q1*q1 - q2*q2 - q3*q3)*x + 2*(q1*q2 - q0*q3)*y + 2*(q1*q3 + q0*q2)*z
-    vy = 2*(q1*q2 + q0*q3)*x + (q0*q0 - q1*q1 + q2*q2 - q3*q3)*y + 2*(q2*q3 - q0*q1)*z
-    vz = 2*(q1*q3 - q0*q2)*x + 2*(q2*q3 + q0*q1)*y + (q0*q0 - q1*q1 - q2*q2 + q3*q3)*z
+    vx = (q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3) * x + 2 * (q1 * q2 - q0 * q3) * y + 2 * (q1 * q3 + q0 * q2) * z
+    vy = 2 * (q1 * q2 + q0 * q3) * x + (q0 * q0 - q1 * q1 + q2 * q2 - q3 * q3) * y + 2 * (q2 * q3 - q0 * q1) * z
+    vz = 2 * (q1 * q3 - q0 * q2) * x + 2 * (q2 * q3 + q0 * q1) * y + (q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3) * z
     return vx, vy, vz
 
 class complement_Filter:
