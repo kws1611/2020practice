0affa5b352a006d4c93470309d828a5b9cf577bd
rladntjd <kws1611@naver.com>
2020-01-22 02:09:14 +0900

    fix

--- covariance_test.py
+++ covariance_test.py
@@ -12,6 +12,13 @@ import time
 import math
 import numpy.linalg as lin
 import tf
+"""
+Q cov 
+0.0299009184657
+0.0315001603291
+0.0751542253079
+0.0964859324556
+"""
 
 def quat_mult(a_1, a_2, a_3, a_4, b_1, b_2, b_3, b_4):
 	q_0 = a_1*b_1 - a_2*b_2 - a_3*b_3 - a_4*b_4
@@ -69,7 +76,8 @@ class kalman_Filter:
 		self.H = np.identity(4)
 		self.Q = 10**(-4)*np.matrix('0.64 0 0 0; 0 0.64 0 0 ; 0 0 0.169 0; 0 0 0 0.64 ')
 		#		self.Q = np.matrix('0 -0.4 -0.65 -0.4; 0.4 0 0.4 -0.65; 0.65 -0.4 0 0.4; 0.4 0.65 -0.4 0')
-		self.R = 10**(-4)*np.matrix('0.5929 0 0 0;0 0.0289 0 0; 0 0 0.0289 0; 0 0 0 0.5929')
+
+		#self.R =np.matrix('0.0209789550647**2 0 0 0;0 0.00541388117932**2 0 0; 0 0 0.0102877460488**2 0; 0 0 0 0.0161420424737**2')
 		# Subscriber created
 		self.mag_x = 0.01
 		self.mag_y = 0.01
@@ -85,9 +93,12 @@ class kalman_Filter:
 		rospy.Subscriber("/mag_raw", MagneticField, self.mag_raw_data)
 		self.Kalman_cov_pub = rospy.Publisher("/pose_covariance",PoseWithCovarianceStamped, queue_size=1)
 		self.Kalman_pub = rospy.Publisher("/Kalman_quat",Quaternion, queue_size=1)
-
-	def get_acc_quat(self):
+		self.Z_saved_w =[]
+		self.Z_saved_x =[]
+		self.Z_saved_y =[]
+		self.Z_saved_z =[] 
 		
+	def get_acc_quat(self):
 		self.ax = self.acc_x / math.sqrt(self.acc_x**2 +self.acc_y**2 + self.acc_z**2)
 		self.ay = self.acc_y / math.sqrt(self.acc_x**2 +self.acc_y**2 + self.acc_z**2)
 		self.az = self.acc_z / math.sqrt(self.acc_x**2 +self.acc_y**2 + self.acc_z**2)
@@ -110,7 +121,6 @@ class kalman_Filter:
 			self.q3_mag = math.sqrt(self.gamma - lx * math.sqrt(self.gamma))/ math.sqrt(2 * self.gamma) 
 			self.q_mag= norm_quat(self.q0_mag, self.q1_mag, self.q2_mag, self.q3_mag)
 	
-
 	def kalman(self):
 		pose_topic = PoseWithCovarianceStamped()
 		kalman_topic = Quaternion()
@@ -123,13 +133,7 @@ class kalman_Filter:
 		self.A = np.identity(4) + self.dt*float(0.5)*np.matrix([[0 ,-self.gyro_x,-self.gyro_y,-self.gyro_z], [self.gyro_x, 0 ,self.gyro_z, -self.gyro_y], [self.gyro_y, -self.gyro_z ,0, self.gyro_x], [self.gyro_z, self.gyro_y ,-self.gyro_x, 0 ]])
 		# Kalman Filter
 		
-		self.Xp = self.A*self.X
-		self.Pp = self.A*self.P*self.A.T +self.Q
-		self.K = self.Pp*self.H.T*lin.inv(self.H*self.Pp*self.H.T + self.R)
-		self.X = self.Xp + self.K*(self.Z - self.H*self.Xp)
-		self.X = norm_quat(self.X[0,0],self.X[1,0],self.X[2,0],self.X[3,0])
-		self.P = self.Pp - self.K*self.H*self.Pp
-
+		self.X = self.A*self.X
 		kalman_topic.x = self.X[0,0]
 		kalman_topic.y = self.X[1,0]
 		kalman_topic.z = self.X[2,0]
@@ -147,7 +151,20 @@ class kalman_Filter:
 		self.Kalman_pub.publish(kalman_topic)
 		self.Kalman_cov_pub.publish(pose_topic)
 		self.rate.sleep()
+		self.Z_saved_w.append(self.X[0,0])
+		self.Z_saved_x.append(self.X[1,0])
+		self.Z_saved_y.append(self.X[2,0])
+		self.Z_saved_z.append(self.X[3,0])
 		
+	def prints(self):
+		std_w = np.std(self.Z_saved_w)
+		std_x = np.std(self.Z_saved_x)
+		std_y = np.std(self.Z_saved_y)
+		std_z = np.std(self.Z_saved_z)
+		print(std_w)
+		print(std_x)
+		print(std_y)
+		print(std_z)
 		
 if __name__ == "__main__":
 
@@ -159,10 +176,12 @@ if __name__ == "__main__":
 	
 		Filtering = kalman_Filter()
 
-		while not rospy.is_shutdown():
-
-			 
+		for i in range(0,10000):
 			Filtering.kalman()
+			
+		Filtering.prints()	
+
+	
 	except rospy.ROSInterruptException: 
 		print "ROS terminated"
 		pass
