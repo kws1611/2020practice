3605b7b0cb3ecc637e4f380c145c3524a2c05548
eeverblue <chicken_00@nate.com>
2020-02-06 15:43:40 +0900

    comp

--- error.py
+++ error.py
@@ -44,19 +44,12 @@ def quat_to_matrix(q0,q1,q2,q3):
 
         return rotation_mat
 
-
-
 class error:
-<<<<<<< HEAD
         def motion_cb(self):
                 A = quat_mult(self.q0_init, -self.q1_init,-self.q2_init, self.q3_init,self.motion_w, self.motion_x, self.motion_y, self.motion_z)
                 self.motion_w, self.motion_x, self.motion_y, self.motion_z = A[0,0], A[0,1], A[0,2], A[0,3]
-        
-        def get_motion_cb(self,msg):
-=======
 
-        def motion_cb(self,msg):
->>>>>>> 884479a0ef4077bfeea0e9e83b74ca047f05d282
+        def get_motion_cb(self,msg):
                 self.mot_msg = msg
                 self.motion_time = self.mot_msg.header.stamp.secs + self.mot_msg.header.stamp.nsecs*10**(-9)
                 self.motion_x = self.mot_msg.pose.orientation.x
